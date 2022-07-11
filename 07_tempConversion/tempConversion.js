92787dc004cf89a093ee68e26e68cee34ebd8b64
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 21:02:29 -0500

    Move code to proper function and fix parameters

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,9 @@
-const ftoc = function(celsius) {
-
-};
-
-const ctof = function(fahrenheit) {
+const ftoc = function(fahrenheit) {
   let celsius = (fahrenheit-32)*5/9;
   return celsius.toFixed(1);
+};
+
+const ctof = function(celsius) {
 };
 
 // Do not edit below this line
