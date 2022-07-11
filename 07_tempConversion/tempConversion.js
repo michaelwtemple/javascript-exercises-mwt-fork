4b5a5f70b80af94199aab98ffd089f0c7d9c8e8f
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 21:01:47 -0500

    Create initial logic for ftoc

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -3,7 +3,8 @@ const ftoc = function(celsius) {
 };
 
 const ctof = function(fahrenheit) {
-
+  let celsius = (fahrenheit-32)*5/9;
+  return celsius.toFixed(1);
 };
 
 // Do not edit below this line
