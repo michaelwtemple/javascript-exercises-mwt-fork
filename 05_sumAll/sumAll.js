ab7b57049b15f67c69a72c7bfce4e020f9135aaa
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 20:42:24 -0500

    Add check for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
 const sumAll = function(start, end) {
     let result = 0;
 
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
     if (start > end) {
         let holder = start;
         start = end;
