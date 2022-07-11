fffdfb566e27fed708d79d488734d200c905f1ae
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 20:38:50 -0500

    Create initial logic for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
 const sumAll = function(start, end) {
+    let result = 0;
+
+    for (let step = start;step <= end; step++) {
+        result += step;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
