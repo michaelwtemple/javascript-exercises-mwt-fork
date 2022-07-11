2f06b6becf6b52511e3a418e5d210ceec489b3fb
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 20:41:08 -0500

    Add logic for larger number first.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 const sumAll = function(start, end) {
     let result = 0;
 
+    if (start > end) {
+        let holder = start;
+        start = end;
+        end = holder;
+    }
+
     for (let step = start;step <= end; step++) {
         result += step;
     }
