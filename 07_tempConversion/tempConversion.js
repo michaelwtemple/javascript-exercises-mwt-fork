7782dfd3d9031bfbb64edbad70a03599c3226602
Michael W. Temple Jr <michaelwtemple@gmail.com>
2022-07-10 21:06:12 -0500

    Create logic to handle 0 result

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
 const ftoc = function(fahrenheit) {
   let celsius = (fahrenheit - 32) * 5 / 9;
+
+  if (celsius == 0)
+    return 0;
+
   return celsius.toFixed(1);
 };
 
 const ctof = function(celsius) {
   let fahrenheit = (celsius * 1.8) + 32;
+
+  if (fahrenheit == 0)
+    return 0;
+    
   return fahrenheit.toFixed(1);
 };
 
